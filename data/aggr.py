0752de51ab87202b9993b042b975fe10b656b921
new account <koch@loyalsock.st.cwi.nl>
2017-05-01 16:07:54 +0200

    Add geojson dow index

--- data/aggr.py
+++ data/aggr.py
@@ -1,5 +1,6 @@
 import simplejson
 import sys
+import arrow
 
 f = open(sys.argv[1])
 x = simplejson.load(f)
@@ -23,9 +24,20 @@ for feature in x['features']:
 
 x['properties']['min_timeslot' ] = min_timeslot
 x['properties']['max_timeslot' ] = max_timeslot
-x['properties']['min_value' ] = min_value
-x['properties']['max_value' ] = max_value
-print (min_timeslot, max_timeslot, min_value, max_value)
+x['properties']['min_value' ] = min_value-0.00000000000001
+x['properties']['max_value' ] = max_value+0.00000000000001
+if x['properties']['starttime'] == 1464804000000:
+    x['properties']['starttime'] = 1464732000000
+print x['properties']
+
+for i in range(min_timeslot,max_timeslot):
+  dt = arrow.get((x['properties']['starttime']/1000) + i*3600).to('Europe/Amsterdam')
+  if dt.hour == 0:
+      key = 'day_'+str(dt.weekday())
+      if key not in x['properties']:
+          x['properties'][key] = i
+
+print x['properties']
 g = open(sys.argv[1],'w')
 simplejson.dump(x, g)
 g.flush()
--- data/aggr_int.py
+++ data/aggr_int.py
@@ -1,5 +1,6 @@
 import simplejson
 import sys
+import arrow
 
 f = open(sys.argv[1])
 x = simplejson.load(f)
@@ -8,12 +9,15 @@ max_timeslot = -999999999999999
 min_value = 999999999999999
 max_value = -999999999999999
 
-if 'properties' not in x:
-   x['properties'] = {}
-   x['source'] = 'UNKNOWN'
-   x['type'] = 'CLOSENESS'
-   x['starttime'] = 1464732000000
 
+x['properties'] = {}
+x['type'] = 'FeatureCollection'
+p = x['properties']
+p['source'] = 'UNKNOWN'
+p['type'] = 'CLOSENESS'
+x['properties']['starttime'] = 1464732000000
+
+print p
 for feature in x['features']:
    for timeslot,value in feature['properties'].items():
        timeslot = int(timeslot)
@@ -31,6 +35,23 @@ x['properties']['min_timeslot' ] = min_timeslot
 x['properties']['max_timeslot' ] = max_timeslot
 x['properties']['min_value' ] = min_value
 x['properties']['max_value' ] = max_value
+
+x['properties']['min_value' ] = min_value-0.00000000000001
+x['properties']['max_value' ] = max_value+0.00000000000001
+if x['properties']['starttime'] == 1464804000000:
+    x['properties']['starttime'] = 1464732000000
+print x['properties']
+
+for i in range(min_timeslot,max_timeslot):
+  dt = arrow.get((x['properties']['starttime']/1000) + i*3600).to('Europe/Amsterdam')
+  if dt.hour == 0:
+      key = 'day_'+str(dt.weekday())
+      if key not in x['properties']:
+          x['properties'][key] = i
+
+print x['properties']
+
+
 print (min_timeslot, max_timeslot, min_value, max_value)
 g = open(sys.argv[1],'w')
 simplejson.dump(x, g)
